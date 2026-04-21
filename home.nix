527ffd9a82fac19c9e8c70f55c5876b11586c269
John Carrijo <johncarrijo2255@gmail.com>
2026-04-21 20:12:36 -0300

    Bloquei de tela

--- home.nix
+++ home.nix
@@ -1,43 +1,44 @@
 { config, pkgs, noctalia, ... }:
 
 {
-  # Importa o módulo do Noctalia
   imports = [ noctalia.homeModules.default ];
 
-  # Versão do estado do home-manager
   home.stateVersion = "25.05";
 
-  # Ativa o Noctalia
-  programs.noctalia-shell = {
-    enable = true;
-  };
-  
+  programs.noctalia-shell.enable = true;
+
   targets.genericLinux.enable = true;
 
   services.swayidle = {
-  enable = true;
-  systemdTargets = [ "graphical-session.target" ];
+    enable = true;
+    systemdTarget = "graphical-session.target";
 
-  events = [
-    {
-      event = "before-sleep";
-      command = "noctalia-shell ipc call lockScreen lock";
-    }
-  ];
+    events = [
+      {
+        event = "before-sleep";
+        command = "${pkgs.noctalia-shell}/bin/noctalia-shell ipc call lockScreen lock";
+      }
+    ];
 
-  timeouts = [
-    {
-      timeout = 300;
-      command = "swaylock -f";
-     }
-   ];
- };  
+    timeouts = [
+      {
+        timeout = 300;
+        command = "swaylock -f";
+      }
+    ];
+  };
 
-  #Abrir APPS Java
+  # 🔥 ESSENCIAL: garantir que swayidle inicia
+  systemd.user.services.swayidle = {
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
+
+  # Java fix (ok manter)
   home.sessionVariables = {
     _JAVA_AWT_WM_NONREPARENTING = "1";
-  };  
+  };
 
-  # Pacotes do usuário (pode deixar vazio)
   home.packages = [ ];
 }
