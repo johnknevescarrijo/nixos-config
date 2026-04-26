1ae1207463a0ba29f78ef1f23e75ab4c38d887d9
John Carrijo <johncarrijo2255@gmail.com>
2026-04-25 21:00:50 -0300

    Adicionando tmux

--- home.nix
+++ home.nix
@@ -12,7 +12,7 @@
   services.swayidle = {
   enable = true;
 
-  systemdTarget = "graphical-session.target";
+  systemdTargets = [ "graphical-session.target"];
 
   timeouts = [
     {
@@ -31,6 +31,47 @@
   home.sessionVariables = {
     _JAVA_AWT_WM_NONREPARENTING = "1";
   };
+
+    # Instalação e configuração do Tmux
+  programs.tmux = {
+    enable = true;
+    clock24 = true;    # Usa o formato de 24 horas
+    # escapeTime = 0;  # Tempo de espera para detectar o prefixo (0 para nenhum delay)
+    # mouse = true;    # Habilita suporte ao mouse (opcional)
+    # shortcut = "a";  # Muda o prefixo para Ctrl+a (padrão é Ctrl+b)
+    extraConfig = ''
+              
+       # Comandos personalizados do tmux vão aqui
+      unbind C-b
+      set -g prefix C-b
+      bind C-b send-prefix
+      
+      # Split
+       bind e split-window -h
+       bind s split-window -v
+
+        # Navegação estilo Vim
+         bind h select-pane -L
+         bind j select-pane -D
+         bind k select-pane -U
+         bind l select-pane -R
+
+         # Trocar janelas
+         bind n next-window
+         bind p previous-window
+
+         # Copy mode (mais fácil)
+         bind m copy-mode
+
+         # Colar
+         bind v paste-buffer
+       
+       
+
+      set -g status-right '#[fg=black,bg=colour15] %H:%M '
+    '';
+  };
+
 
   home.packages = [ ];
 }
